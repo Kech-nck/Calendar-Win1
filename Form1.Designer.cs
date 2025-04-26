7dddeec0cbb12befa575f9f0d5291084e60f1c2b
Kech-nck <aleshakech977@gmail.com>
2025-04-26 13:43:21 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,15 +39,15 @@
             timer1 = new System.Windows.Forms.Timer(components);
             label6 = new Label();
             label7 = new Label();
+            label8 = new Label();
             timer2 = new System.Windows.Forms.Timer(components);
             button2 = new Button();
-            button3 = new Button();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(153, 11);
+            label1.Location = new Point(230, 63);
             label1.Name = "label1";
             label1.Size = new Size(92, 15);
             label1.TabIndex = 0;
@@ -55,7 +55,7 @@
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(123, 35);
+            monthCalendar1.Location = new Point(200, 87);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 1;
             monthCalendar1.DateChanged += monthCalendar1_DateChanged;
@@ -63,7 +63,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(9, 256);
+            label3.Location = new Point(86, 348);
             label3.Name = "label3";
             label3.Size = new Size(116, 15);
             label3.TabIndex = 3;
@@ -71,14 +71,14 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(136, 248);
+            textBox1.Location = new Point(213, 345);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(207, 23);
             textBox1.TabIndex = 4;
             // 
             // button1
             // 
-            button1.Location = new Point(178, 302);
+            button1.Location = new Point(247, 384);
             button1.Name = "button1";
             button1.Size = new Size(128, 23);
             button1.TabIndex = 5;
@@ -90,24 +90,28 @@
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(349, 86);
+            listBox1.Location = new Point(426, 138);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(310, 274);
+            listBox1.Size = new Size(369, 274);
             listBox1.TabIndex = 6;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(349, 68);
+            label4.Location = new Point(480, 114);
             label4.Name = "label4";
-            label4.Size = new Size(136, 15);
+            label4.Size = new Size(133, 15);
             label4.TabIndex = 7;
-            label4.Text = "Добавленные события:";
+            label4.Text = "Добавленные события";
+            // 
+            // timer1
+            // 
+            
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(114, 219);
+            label6.Location = new Point(191, 271);
             label6.Name = "label6";
             label6.Size = new Size(0, 15);
             label6.TabIndex = 10;
@@ -115,10 +119,18 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(249, 219);
+            label7.Location = new Point(326, 271);
             label7.Name = "label7";
             label7.Size = new Size(0, 15);
             label7.TabIndex = 11;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(654, 114);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 15);
+            label8.TabIndex = 12;
             // 
             // timer2
             // 
@@ -126,31 +138,21 @@
             // 
             // button2
             // 
-            button2.Location = new Point(9, 289);
+            button2.Location = new Point(86, 384);
             button2.Name = "button2";
             button2.Size = new Size(133, 23);
             button2.TabIndex = 13;
-            button2.Text = "Сохранить события";
+            button2.Text = "Сохранить событие";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(9, 318);
-            button3.Name = "button3";
-            button3.Size = new Size(133, 23);
-            button3.TabIndex = 14;
-            button3.Text = "Загрузить события";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(672, 370);
-            Controls.Add(button3);
+            ClientSize = new Size(800, 450);
             Controls.Add(button2);
+            Controls.Add(label8);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label4);
@@ -161,7 +163,7 @@
             Controls.Add(monthCalendar1);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Календарь";
+            Text = "Form1";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -180,8 +182,8 @@
         private Label label5;
         private Label label6;
         private Label label7;
+        private Label label8;
         private System.Windows.Forms.Timer timer2;
         private Button button2;
-        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,90 +1,72 @@
 using System.Text;
-using static System.Windows.Forms.LinkLabel;
 
 namespace WinFormsApp4
 {
     public partial class Form1 : Form
     {
-        List<string> list = new(); //массив событий для сохранений
-        List<string> eventsDate = new(); //массив событий для уведомлений
-        string eventsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "events.txt"); //путь к файлу сохранения
+        List<string> list = new List<string>();
+        List<string> eventsList = new List<string>();
+        string eventsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "events.txt");
 
         public Form1()
         {
             InitializeComponent();
             monthCalendar1.DateChanged += monthCalendar1_DateChanged;
-
-            label6.Text = DateTime.Now.ToString("D"); //Начальные значения label6 и label7
-            label7.Text = DateTime.Now.ToString("T");
-
-            timer1.Interval = 1000;
-            timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Start();
-
+            
             timer2.Interval = 14000;
             timer2.Tick += new EventHandler(timer2_Tick);
             timer2.Start();
+
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
         {
-            label6.Text = String.Format($"{e.Start.ToLongDateString()}"); //label6 меняется на выбранную даты календаря
+            label6.Text = String.Format($"{e.Start.ToLongDateString()}");
         }
+
 
         private void button1_Click(object sender, EventArgs e)
-        {            
-            listBox1.Items.Add($"{label6.Text} - {textBox1.Text}"); //дата и имя события вводятся в listbox1
-            list.Add($"{label6.Text} - {textBox1.Text}"); //добавление события в массивы
-            eventsDate.Add(label6.Text);
-        }
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                if (File.Exists(eventsFilePath))
+                {
+                    var allEvents = File.ReadAllLines(eventsFilePath);
+                    if (allEvents.Length > 0)
+                    {
+                        Random rnd = new Random();
+                        string randomEvent = allEvents[rnd.Next(allEvents.Length)];
+                        listBox1.Items.Add($"{label6.Text} - {randomEvent}");
+                        listBox1.Refresh();
+                        eventsList.Add(randomEvent);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл событий пуст", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            foreach (string item in list) //читает события из массива list
-            {
-                try
-                {
-                    string newEvent = item;
-                    File.AppendAllText(eventsFilePath, newEvent + Environment.NewLine); //сохранение события в файл
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Ошибка сохранения события!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-            MessageBox.Show("События были сохранены.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            
-        }
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(eventsFilePath))
-            {
-                var allEvents = File.ReadAllLines(eventsFilePath); //чтение файла сохранения
-                if (allEvents.Length > 0)
-                {
-                    listBox1.Items.AddRange(allEvents); //добавление всех событий из файла
                 }
                 else
                 {
-                    MessageBox.Show("Файл событий пуст", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Файл событий не найден", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-
             }
             else
             {
-                MessageBox.Show("Файл событий не найден", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label8.Text = label6.Text;
+                listBox1.Items.Add($"{label6.Text} - {textBox1.Text}");
+                listBox1.Refresh();
+                eventsList.Add(textBox1.Text);
+                list.Add(label8.Text);
             }
-        }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            label7.Text = DateTime.Now.ToString("T"); //Держит текущее время в label7
+
         }
 
         private void timer2_Tick(object sender, EventArgs e)
         {
-            string dateNow = DateTime.Now.ToString("D"); //текущая дата
-            int count = 0; //количество событий на сегодня
-            foreach (string item in eventsDate) //сравнивает даты из массива eventsDate
+            string dateNow = DateTime.Now.ToShortDateString();
+            int count = 0;
+            foreach (string item in list)
             {
                 if (item == dateNow)
                 {
@@ -101,8 +83,30 @@ namespace WinFormsApp4
             {
                 MessageBox.Show($"{count} cобытия на сегодня ({dateNow}) уже начались!");
             }
+
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                try
+                {
+                    string newEvent = $"{textBox1.Text}";
+                    File.AppendAllText(eventsFilePath, newEvent + Environment.NewLine);
+                    MessageBox.Show("Событие успешно сохранено в файл!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка сохранения: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Введите текст события!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
         
     }
 }
